dd7553d1e01a4f0d7d8673cbdf5e0d27f84366dc
Maxim Liven <maxlevs@mail.ru>
2021-02-18 18:58:56 +0300

    meh

--- MCServerNotifier/Program.cs
+++ MCServerNotifier/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
+using System.Threading;
 using MCQueryLib;
 
 namespace MCServerNotifier
@@ -8,10 +10,44 @@ namespace MCServerNotifier
     {
         static void Main(string[] args)
         {
-            var test = new McQuery(IPAddress.Loopback, 25565);
-            var bstat = test.GetFullStat();
-            Console.WriteLine("["+string.Join(' ', bstat.Players)+"]");
-            return;
+            var mcQuery = new McQuery(IPAddress.Loopback, 25565);
+            var delayTime = 1000;
+            bool? isOnline = null;
+            string[] playerBuff = null;
+            
+            while (true)
+            {
+                var fullStat = mcQuery.GetFullStat();
+                
+                if (playerBuff != null)
+                {
+                    foreach (var player in fullStat.Players)
+                    {
+                        if (!playerBuff.Contains(player))
+                        {
+                            Console.WriteLine($"[{player}] has joined the game");
+                        }
+                    }
+                    
+                    foreach (var player in playerBuff)
+                    {
+                        if (!fullStat.Players.Contains(player))
+                        {
+                            Console.WriteLine($"[{player}] has left the game");
+                        }
+                    }
+                }
+
+                if (isOnline != null && isOnline != mcQuery.Online)
+                {
+                    Console.WriteLine($"Server is online? [{mcQuery.Online}]");
+                }
+                isOnline = mcQuery.Online;
+
+                playerBuff = fullStat.Players;
+                
+                Thread.Sleep(delayTime);
+            }
         }
     }
 }
